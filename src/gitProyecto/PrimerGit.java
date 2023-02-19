6ff2f37644a68850f5ab9f1121a110974689cba7
Mery <mchozamerino@gmail.com>
2023-02-19 13:08:32 +0100

    Mostramos la suma por pantalla

--- src/gitProyecto/PrimerGit.java
+++ src/gitProyecto/PrimerGit.java
@@ -5,9 +5,9 @@ public class PrimerGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		int suma = 4+5;
 		System.out.println("Hola cara pelota");
-		System.out.println(4+5);
+		System.out.println(suma);
 	}
 
 }
