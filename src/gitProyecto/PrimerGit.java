10b9f841819948931e6979825b7d6f75bd06f7fd
Mery <mchozamerino@gmail.com>
2023-02-19 13:03:08 +0100

    creamos clase PrimerGit

---
+++ src/gitProyecto/PrimerGit.java
@@ -0,0 +1,12 @@
+package gitProyecto;
+
+public class PrimerGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola cara pelota");
+	}
+
+}
