61a97cb9c205ce054f140d821a3423ed7783507d
Mery <mchozamerino@gmail.com>
2023-02-19 20:51:23 +0100

    por favor funciona

--- src/gitProyecto/PrimerGit.java
+++ src/gitProyecto/PrimerGit.java
@@ -5,7 +5,7 @@ public class PrimerGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int suma = 4+5;
+		int suma = 4+6;
 		System.out.println("Hola cara pelota");
 		System.out.println(suma);
 	}
